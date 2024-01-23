20bda7df7d22ff602d04ac09c826f0fac5190052
“davidmagbee” <david.magbee@me.com>
2024-01-22 20:46:24 -0500

    player setup with initial tracks added for testing

--- app/page.js
+++ app/page.js
@@ -1,10 +1,26 @@
-import Header from "@/components/Header";
+'use client'
 
+import { useState } from "react";
+import Header from "@/components/Header";
+import Tracks from "@/components/Tracks";
 
 export default function Home() {
+  const [selectedHowl, setSelectedHowl] = useState(null);
+  const [playing, setPlaying] = useState(false);
+
   return (
     <div>
-      <Header />
+      <Header 
+        playing={playing}
+        setPlaying={setPlaying}
+        selectedHowl={selectedHowl}
+      />
+      <Tracks 
+        playing={playing}
+        setPlaying={setPlaying}
+        selectedHowl={selectedHowl}
+        setSelectedHowl={setSelectedHowl}
+      />
     </div>
   );
 }
--- components/Tracks.js
+++ components/Tracks.js
@@ -1,1 +1,77 @@
-``
+import React from "react"
+import TableHeader from "./TableHeader"
+import Track from "./Track"
+
+const tracks = [
+    {
+      id: 1,
+      src: "helloNeighbor.mp3",
+      title: "Hello There Neighbor - Call Sign",
+      artist: "DJ Fuzz",
+      album: "Better Radio Call Signs",
+      imgSrc: "https://unsplash.it/40/40",
+      duration: "0:10",
+    },
+    {
+      id: 2,
+      src: "turnItUp.mp3",
+      title: "Turn It Up",
+      artist: "DJ Fuzz",
+      album: "Better Radio Call Signs",
+      imgSrc: "https://unsplash.it/40/40",
+      duration: "3:52",
+    },
+    {
+      id: 3,
+      src: "oldManCallSign.mp3",
+      title: "Old Man - Call Sign",
+      artist: "DJ Fuzz",
+      album: "Better Radio Call Signs",
+      imgSrc: "https://unsplash.it/40/40",
+      duration: "0:28",
+    },
+    {
+        id: 4,
+        src: "cheeboStarsOut.mp3",
+        title: "Smoother AF",
+        artist: "Cheebo",
+        album: "Better Radio Call Signs",
+        imgSrc: "https://unsplash.it/40/40",
+        duration: "0:31",
+      },
+      {
+        id: 5,
+        src: "bellmanReallyRocks.mp3",
+        title: "The Only Station That Really Rocks",
+        artist: "Nick Bellman",
+        album: "Better Radio Call Signs",
+        imgSrc: "https://unsplash.it/40/40",
+        duration: "0:10",
+      },
+  ];
+
+export default function Tracks ({
+    playing,
+    setPlaying,
+    selectedHowl,
+    setSelectedHowl,
+}) {
+    return (
+        <div className="w-full mx-auto px-4 py-2">
+            <TableHeader />
+           {tracks.map((track, idx) => {
+            return (
+                <Track  
+                    key={track.id}
+                    track={track}
+                    selectedHowl={selectedHowl}
+                    setSelectedHowl={setSelectedHowl}
+                    playing={playing}
+                    setPlaying={setPlaying}
+                    isFirstTrack={idx === 0}
+                />
+            )
+           })}
+        </div>
+    )
+}
